4b8b7fe29ea10bf01ba885473d1415bd9b096a0c
Percy <Percy@DESKTOP-BUE192R>
2022-09-03 21:39:28 -0500

    Agregar archivos de proyecto.

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { Id = 1, Nombre = "Felipe" },
+                new Autor() { Id = 2, Nombre = "Fernanda" }
+
+            };
+        }
+    }
+}
---
+++ WebApiAutores/Entidades/Autor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebApiAutores.Entidades
+{
+    public class Autor
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; }  
+    }
+}
